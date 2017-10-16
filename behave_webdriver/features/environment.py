dde5393492c4c8835ae54837cdafce63ebac78ef
Young,Spencer P <spenceryoung@ufl.edu>
2017-10-16 10:26:06 -0400

    cleanup environment.py

--- behave_webdriver/features/environment.py
+++ behave_webdriver/features/environment.py
@@ -1,17 +1,6 @@
-import os
-from selenium import webdriver
-
-
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.common.action_chains import ActionChains
-import time
-
 from behave_webdriver import BehaveDriver
 
 def before_all(context):
     context.behave_driver = BehaveDriver.headless_chrome()
-
 def after_all(context):
     context.behave_driver.quit()
