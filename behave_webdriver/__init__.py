1b967988238f3b9ad107c219d3c1d720254f6b71
Young,Spencer P <spenceryoung@ufl.edu>
2017-10-16 17:08:39 -0400

    change keys implementation

--- behave_webdriver/__init__.py
+++ behave_webdriver/__init__.py
@@ -6,73 +6,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
 import time
 
-KEYS = {
- 'ADD': '\ue025',
- 'ALT': '\ue00a',
- 'ARROW_DOWN': '\ue015',
- 'ARROW_LEFT': '\ue012',
- 'ARROW_RIGHT': '\ue014',
- 'ARROW_UP': '\ue013',
- 'BACKSPACE': '\ue003',
- 'BACK_SPACE': '\ue003',
- 'CANCEL': '\ue001',
- 'CLEAR': '\ue005',
- 'COMMAND': '\ue03d',
- 'CONTROL': '\ue009',
- 'DECIMAL': '\ue028',
- 'DELETE': '\ue017',
- 'DIVIDE': '\ue029',
- 'DOWN': '\ue015',
- 'END': '\ue010',
- 'ENTER': '\ue007',
- 'EQUALS': '\ue019',
- 'ESCAPE': '\ue00c',
- 'F1': '\ue031',
- 'F10': '\ue03a',
- 'F11': '\ue03b',
- 'F12': '\ue03c',
- 'F2': '\ue032',
- 'F3': '\ue033',
- 'F4': '\ue034',
- 'F5': '\ue035',
- 'F6': '\ue036',
- 'F7': '\ue037',
- 'F8': '\ue038',
- 'F9': '\ue039',
- 'HELP': '\ue002',
- 'HOME': '\ue011',
- 'INSERT': '\ue016',
- 'LEFT': '\ue012',
- 'LEFT_ALT': '\ue00a',
- 'LEFT_CONTROL': '\ue009',
- 'LEFT_SHIFT': '\ue008',
- 'META': '\ue03d',
- 'MULTIPLY': '\ue024',
- 'NULL': '\ue000',
- 'NUMPAD0': '\ue01a',
- 'NUMPAD1': '\ue01b',
- 'NUMPAD2': '\ue01c',
- 'NUMPAD3': '\ue01d',
- 'NUMPAD4': '\ue01e',
- 'NUMPAD5': '\ue01f',
- 'NUMPAD6': '\ue020',
- 'NUMPAD7': '\ue021',
- 'NUMPAD8': '\ue022',
- 'NUMPAD9': '\ue023',
- 'PAGE_DOWN': '\ue00f',
- 'PAGE_UP': '\ue00e',
- 'PAUSE': '\ue00b',
- 'RETURN': '\ue006',
- 'RIGHT': '\ue014',
- 'SEMICOLON': '\ue018',
- 'SEPARATOR': '\ue026',
- 'SHIFT': '\ue008',
- 'SPACE': '\ue00d',
- 'SUBTRACT': '\ue027',
- 'TAB': '\ue004',
- 'UP': '\ue013'}
 
 class element_is_enabled(object):
     def __init__(self, element):
@@ -262,8 +199,8 @@ class BehaveDriver(object):
         actions.perform()
 
     def press_button(self, button):
-        if len(button) > 1 and button.upper() in KEYS:
-            button = KEYS.get(button.upper())
+        if len(button) > 1:
+            button = getattr(Keys, button.upper(), button)
         self.send_keys(button)
 
     def scroll_to_bottom(self):
