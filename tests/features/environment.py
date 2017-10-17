b7a36672680177fcd130c833f4cb2e72ca3b6fb5
spyoungtech <spencer.young@spyoung.com>
2017-10-17 19:33:28 -0400

    test firefox

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -5,5 +5,6 @@ def before_all(context):
     browser_env = os.getenv('BEHAVE_WEBDRIVER', 'headless_chrome').lower()
     driver = getattr(BehaveDriver, browser_env, BehaveDriver.headless_chrome)
     context.behave_driver = driver()
+    print("DRIVER: {}".format(context.behave_driver.name))
 def after_all(context):
     context.behave_driver.quit()
