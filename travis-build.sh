fd111da58674ca089e1c350a3e5fe6285e7d5a36
spyoungtech <spencer.young@spyoung.com>
2017-10-18 02:40:37 -0400

    test travis build

--- travis-build.sh
+++ travis-build.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-if [ "${BEHAVE_WEBDRIVER}" = "chrome" ] || [ "${BEHAVE_WEBDRIVER}" = "headless_chrome"]; then
+if [ "${BEHAVE_WEBDRIVER}" = "chrome" ] || [ "${BEHAVE_WEBDRIVER}" = "headless_chrome" ]; then
     wget -N http://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip -P ~/
     unzip ~/chromedriver_linux64.zip -d ~/
     rm ~/chromedriver_linux64.zip
@@ -10,7 +10,7 @@ if [ "${BEHAVE_WEBDRIVER}" = "chrome" ] || [ "${BEHAVE_WEBDRIVER}" = "headless_c
     chmod +x /usr/local/share/chromedriver
     ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
 
-elif [ "${BEHAVE_WEBDRIVER}" = "firefox"]; then
+elif [ "${BEHAVE_WEBDRIVER}" = "firefox" ]; then
     wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz
     tar -xvzf ./geckodriver-v0.11.1-linux64.tar.gz
     rm ./geckodriver-v0.11.1-linux64.tar.gz
