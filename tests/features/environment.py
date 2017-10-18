9f7d017c9badccb7ab0b7678e9a8d84d1896c79d
spyoungtech <spencer.young@spyoung.com>
2017-10-18 04:01:45 -0400

    remove headed chrome

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -1,6 +1,8 @@
 import os
 from behave_webdriver import BehaveDriver
 
+
+
 def before_all(context):
     browser_env = os.getenv('BEHAVE_WEBDRIVER', 'headless_chrome').lower()
     driver = getattr(BehaveDriver, browser_env, BehaveDriver.headless_chrome)
