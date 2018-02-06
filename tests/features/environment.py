828f8f97e308cda1293d2f3a1abbc8d5d6ab7e6d
spyoungtech <spencer.young@spyoung.com>
2018-02-06 10:35:41 -0500

    don't maximize; remove debug prints

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -5,10 +5,7 @@ from behave_webdriver import BehaveDriver
 
 def before_all(context):
     browser_env = os.getenv('BEHAVE_WEBDRIVER', 'headless_chrome').lower()
-    print(browser_env)
     driver = getattr(BehaveDriver, browser_env, BehaveDriver.headless_chrome)
     context.behave_driver = driver()
-    context.behave_driver.maximize_window()
-    print("DRIVER: {}".format(context.behave_driver.name))
 def after_all(context):
     context.behave_driver.quit()
