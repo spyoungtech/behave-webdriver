4baaf2baa809b46aafcf96428b19486b6788364e
spyoungtech <spencer.young@spyoung.com>
2018-03-08 13:50:55 -0500

    :construction_worker: tests

--- tests/unittests/test_from.py
+++ tests/unittests/test_from.py
@@ -8,17 +8,7 @@ sys.path.insert(0, root_dir)
 import behave_webdriver
 
 driver_names = ['Chrome', 'Firefox', 'Ie', 'Edge', 'Opera', 'Safari', 'BlackBerry', 'PhantomJS', 'Android', 'Remote']
-# drivers = [behave_webdriver.Chrome,
-#            behave_webdriver.Firefox,
-#            behave_webdriver.Ie,
-#            behave_webdriver.Edge,
-#            behave_webdriver.Opera,
-#            behave_webdriver.Safari,
-#            behave_webdriver.BlackBerry,
-#            behave_webdriver.PhantomJS,
-#            behave_webdriver.Android,
-#            behave_webdriver.Remote]
-#driver_map = [(driver_name, driver) for driver_name, driver in zip(driver_names, drivers)]
+
 
 @pytest.mark.parametrize("driver_name", driver_names)
 def test_browser_from_string(driver_name):
@@ -27,10 +17,31 @@ def test_browser_from_string(driver_name):
         driver = behave_webdriver.from_string(driver_name)
         assert mock_driver.called
 
+
 @pytest.mark.parametrize("driver_name", driver_names)
 def test_browser_from_env(driver_name):
     os.environ['BEHAVE_WEBDRIVER'] = driver_name
     driver_qual_name = 'behave_webdriver.' + driver_name
     with mock.patch(driver_qual_name) as mock_driver:
-        driver = behave_webdriver.from_string(driver_name)
+        driver = behave_webdriver.from_env()
         assert mock_driver.called
+
+
+def test_default_driver_as_driver():
+    def_driver = behave_webdriver.Chrome
+    Driver = behave_webdriver.from_env(default_driver=def_driver)
+    assert Driver is def_driver
+
+
+def test_env_raises_for_invalid_drivername():
+    os.environ['BEHAVE_WEBDRIVER'] = 'foo'
+    with pytest.raises(ValueError) as excinfo:
+        driver = behave_webdriver._from_env()
+    assert "No driver found in environment variables and no default" in str(excinfo.value)
+
+
+def test_string_raises_for_invalid_drivername_and_contains_options():
+    with pytest.raises(ValueError) as excinfo:
+        driver = behave_webdriver._from_string('foo')
+    assert 'No such driver "foo"' in str(excinfo.value)
+    assert all(dname.upper() in str(excinfo.value) for dname in driver_names)
