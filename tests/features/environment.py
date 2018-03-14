7caf21510316d63dda5649c9cd297258b29718c2
Spencer Young <spencer.young@spyoung.com>
2018-03-14 17:31:56 -0400

    add firefox support (#48)
    add preliminary support for firefox
    
    update travis build to test firefox
    
    modifies some tests for firefox; implement @skip_firefox tag

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -8,17 +8,20 @@ from functools import partial
 def before_all(context):
     kwargs = {'default_wait': 5}
 
-    Driver = behave_webdriver._from_env(default_driver=behave_webdriver.Chrome.headless)
-    if Driver == behave_webdriver.Chrome.headless:
+    Driver = behave_webdriver._from_env()
+    if Driver == behave_webdriver.Chrome:
         opts = ChromeOptions()
         opts.add_argument('--no-sandbox')  # for travis build
         kwargs['chrome_options'] = opts
-        pwd_chrome_path = os.path.abspath(os.path.join(os.getcwd(), 'chromedriver'))
-        if sys.version_info[0] < 3:
-            ex_path = pwd_chrome_path
-        else:
-            ex_path = shutil.which('chromedriver') or pwd_chrome_path
-        kwargs['executable_path'] = ex_path
+
+    pwd_driver_path = os.path.abspath(os.path.join(os.getcwd(), Driver._driver_name))
+    if sys.version_info[0] < 3:
+        ex_path = pwd_driver_path
+    else:
+        ex_path = shutil.which(Driver._driver_name) or pwd_driver_path
+    kwargs['executable_path'] = ex_path
+    if os.environ.get('BEHAVE_WEBDRIVER_HEADLESS', None) and hasattr(Driver, 'headless'):
+        Driver = Driver.headless
     context.BehaveDriver = partial(Driver, **kwargs)
     context.behave_driver = context.BehaveDriver()
 
@@ -31,3 +34,8 @@ def before_feature(context, feature):
         context.behave_driver.quit()
         context.behave_driver = context.BehaveDriver()
         context.behave_driver.default_wait = 5
+
+def before_scenario(context, scenario):
+    if "skip_firefox" in scenario.effective_tags and os.environ.get("BEHAVE_WEBDRIVER", '').lower() == 'firefox':
+        scenario.skip("Skipping because @skip_firefox tag (usually this is because of a known-issue with firefox)")
+        return
--- tests/unittests/test_from.py
+++ tests/unittests/test_from.py
@@ -12,36 +12,44 @@ driver_names = ['Chrome', 'Firefox', 'Ie', 'Edge', 'Opera', 'Safari', 'BlackBerr
 
 @pytest.mark.parametrize("driver_name", driver_names)
 def test_browser_from_string(driver_name):
-    driver_qual_name = 'behave_webdriver.' + driver_name
-    with mock.patch(driver_qual_name) as mock_driver:
-        driver = behave_webdriver.from_string(driver_name)
-        assert mock_driver.called
+    with mock.patch.dict(os.environ, {'BEHAVE_WEBDRIVER_HEADLESS': '0'}):
+        driver_qual_name = 'behave_webdriver.' + driver_name
+        with mock.patch(driver_qual_name) as mock_driver:
+            driver = behave_webdriver.from_string(driver_name)
+            assert mock_driver.called
 
 
 @pytest.mark.parametrize("driver_name", driver_names)
 def test_browser_from_env(driver_name):
-    driver_qual_name = 'behave_webdriver.' + driver_name
-    with mock.patch.dict(os.environ, {'BEHAVE_WEBDRIVER': driver_name}), mock.patch(driver_qual_name) as mock_driver:
-        driver = behave_webdriver.from_env()
-        assert mock_driver.called
+    with mock.patch.dict(os.environ, {'BEHAVE_WEBDRIVER_HEADLESS': '0'}):
+        driver_qual_name = 'behave_webdriver.' + driver_name
+        with mock.patch.dict(os.environ, {'BEHAVE_WEBDRIVER': driver_name}), mock.patch(driver_qual_name) as mock_driver:
+            driver = behave_webdriver.from_env()
+            assert mock_driver.called
 
 
 def test_default_from_env_driver_as_driver():
-    def_driver = behave_webdriver.Chrome
-    Driver = behave_webdriver._from_env(default_driver=def_driver)
-    assert Driver is def_driver
+    with mock.patch.dict(os.environ, {'BEHAVE_WEBDRIVER': 'deleteme'}):
+        del os.environ['BEHAVE_WEBDRIVER']
+        def_driver = behave_webdriver.Chrome
+        Driver = behave_webdriver._from_env(default_driver=def_driver)
+        assert Driver is def_driver
 
 
 def test_default_from_env_driver_as_string():
-    expected_driver = behave_webdriver.Chrome
-    Driver = behave_webdriver._from_env(default_driver='Chrome')
-    assert Driver is expected_driver
+    with mock.patch.dict(os.environ, {'BEHAVE_WEBDRIVER': 'deleteme'}):
+        del os.environ['BEHAVE_WEBDRIVER']
+        expected_driver = behave_webdriver.Chrome
+        Driver = behave_webdriver._from_env(default_driver='Chrome')
+        assert Driver is expected_driver
 
 
 def test_env_raises_for_absent_drivername():
-    with pytest.raises(ValueError) as excinfo:
-        driver = behave_webdriver._from_env()
-    assert "No driver found in environment variables and no default" in str(excinfo.value)
+    with mock.patch.dict(os.environ, {'BEHAVE_WEBDRIVER': 'deleteme'}):
+        del os.environ['BEHAVE_WEBDRIVER']
+        with pytest.raises(ValueError) as excinfo:
+            driver = behave_webdriver._from_env()
+        assert "No driver found in environment variables and no default" in str(excinfo.value)
 
 
 def test_string_raises_for_invalid_drivername_and_contains_options():
