bc8b25d52ad68816718a331faeb9b3bf96b227eb
Cyrille Pontvieux <cpontvieux@systra.com>
2018-04-26 18:14:01 +0200

    Provide fixture and before_xxx functions

--- behave_webdriver/__init__.py
+++ behave_webdriver/__init__.py
@@ -1,6 +1,21 @@
-__all__ = ['Chrome', 'Firefox', 'Ie', 'Edge', 'Opera', 'Safari', 'BlackBerry', 'PhantomJS', 'Android', 'Remote']
-import os
-import behave_webdriver.driver
+__all__ = [
+    'Chrome',
+    'Firefox',
+    'Ie',
+    'Edge',
+    'Opera',
+    'Safari',
+    'BlackBerry',
+    'PhantomJS',
+    'Android',
+    'Remote',
+    'from_env',
+    'from_string',
+    'fixture_browser,'
+    'before_all_factory',
+    'before_feature_factory',
+    'before_scenario_factory',
+]
 from behave_webdriver.driver import (Chrome,
                                      Firefox,
                                      Ie,
@@ -11,42 +26,9 @@ from behave_webdriver.driver import (Chrome,
                                      PhantomJS,
                                      Android,
                                      Remote)
-
-
-
-def _from_string(webdriver_string):
-    drivers = [Chrome, Firefox, Ie, Edge, Opera, Safari, BlackBerry, PhantomJS, Android, Remote]
-    driver_map = dict(zip((name.upper() for name in __all__), drivers))
-    driver_map['CHROME.HEADLESS'] = Chrome.headless
-
-    Driver = driver_map.get(webdriver_string.upper(), None)
-    if Driver is None:
-        raise ValueError('No such driver "{}". Valid options are: {}'.format(webdriver_string,
-                                                                             ', '.join(driver_map.keys())))
-    return Driver
-
-
-def from_string(webdriver_string, *args, **kwargs):
-    Driver = _from_string(webdriver_string)
-    return Driver(*args, **kwargs)
-
-
-def _from_env(default_driver=None):
-    browser_env = os.getenv('BEHAVE_WEBDRIVER', default_driver)
-    if browser_env is None:
-        raise ValueError('No driver found in environment variables and no default driver selection')
-    if isinstance(browser_env, str):
-        Driver = _from_string(browser_env)
-    else:
-        # if not a string, assume we have a webdriver instance
-        Driver = browser_env
-    return Driver
-
-
-def from_env(*args, **kwargs):
-    default_driver = kwargs.pop('default_driver', None)
-    if default_driver is None:
-        default_driver = 'Chrome.headless'
-    Driver = _from_env(default_driver=default_driver)
-
-    return Driver(*args, **kwargs)
+from behave_webdriver.utils import (from_env,
+                                    from_string)
+from behave_webdriver.fixtures import (fixture_browser,
+                                       before_all_factory,
+                                       before_feature_factory,
+                                       before_scenario_factory)
---
+++ behave_webdriver/utils.py
@@ -0,0 +1,50 @@
+from os import getenv
+from behave_webdriver.driver import (Chrome,
+                                     Firefox,
+                                     Ie,
+                                     Edge,
+                                     Opera,
+                                     Safari,
+                                     BlackBerry,
+                                     PhantomJS,
+                                     Android,
+                                     Remote)
+
+
+def _from_string(webdriver_string):
+    def get_driver_name(driver):
+        return driver.__name__.upper()
+    drivers = [Chrome, Firefox, Ie, Edge, Opera, Safari, BlackBerry, PhantomJS, Android, Remote]
+    driver_map = {get_driver_name(d): d for d in drivers}
+    driver_map['CHROME.HEADLESS'] = Chrome.headless
+    Driver = driver_map.get(webdriver_string.upper(), None)
+    if Driver is None:
+        raise ValueError('No such driver "{}". Valid options are: {}'.format(webdriver_string,
+                                                                             ', '.join(driver_map.keys())))
+    return Driver
+
+
+def from_string(webdriver_string, *args, **kwargs):
+    Driver = _from_string(webdriver_string)
+    return Driver(*args, **kwargs)
+
+
+def _from_env(default_driver=None):
+    browser_env = getenv('BEHAVE_WEBDRIVER', default_driver)
+    if browser_env is None:
+        raise ValueError('No driver found in environment variables and no default driver selection')
+    if isinstance(browser_env, str):
+        Driver = _from_string(browser_env)
+    else:
+        # if not a string, assume we have a webdriver instance
+        Driver = browser_env
+    return Driver
+
+
+def from_env(*args, **kwargs):
+    default_driver = kwargs.pop('default_driver', None)
+    if default_driver is None:
+        default_driver = 'Chrome.headless'
+    Driver = _from_env(default_driver=default_driver)
+
+    return Driver(*args, **kwargs)
