32d74ebc9b15a842aae63ded129472e5880cba63
Spencer Young <spencer.young@spyoung.com>
2018-12-08 02:14:38 -0800

    remove tests of old implementation

--- tests/unittests/test_fixture.py
+++ tests/unittests/test_fixture.py
@@ -8,69 +8,6 @@ sys.path.insert(0, root_dir)
 import behave_webdriver.fixtures
 
 
-def test_driver_from_env():
-    with mock.patch('behave_webdriver.fixtures._from_env') as mock_from_env:
-        chrome = mock.MagicMock(name='chrome')
-        Chrome = mock.MagicMock(name='Chrome', return_value=chrome)
-        mock_from_env.return_value = Chrome
-        ctx = mock.MagicMock()
-        gen = behave_webdriver.fixtures.fixture_browser(ctx)
-        driver = next(gen)
-        assert driver is chrome
-        assert ctx.behave_driver is chrome
-        assert mock_from_env.called
-        try:
-            next(gen)
-            pytest.fail('StopIteration expected')
-        except StopIteration:
-            pass
-        assert chrome.quit.called
-        assert 'behave_driver' not in ctx
-
-
-def test_driver_from_name():
-    with mock.patch('behave_webdriver.fixtures._from_string') as mock_from_string:
-        chrome = mock.MagicMock(name='chrome')
-        Chrome = mock.MagicMock(name='Chrome', return_value=chrome)
-        mock_from_string.return_value = Chrome
-        ctx = mock.MagicMock()
-        gen = behave_webdriver.fixtures.fixture_browser(ctx, webdriver='chrome')
-        driver = next(gen)
-        assert driver is chrome
-        assert ctx.behave_driver is chrome
-        assert mock_from_string.called_with('chrome')
-        try:
-            next(gen)
-            pytest.fail('StopIteration expected')
-        except StopIteration:
-            pass
-        assert chrome.quit.called
-        assert 'behave_driver' not in ctx
-
-
-def test_driver_from_good_class():
-    class CustomDriver(behave_webdriver.fixtures.BehaveDriverMixin):
-        def __init__(self, *args, **kwargs):
-            self._args = args
-            self._kwargs = kwargs
-            self._quit = False
-
-        def quit(self):
-            self._quit = True
-    ctx = mock.MagicMock()
-    gen = behave_webdriver.fixtures.fixture_browser(ctx, webdriver=CustomDriver)
-    driver = next(gen)
-    assert isinstance(driver, CustomDriver)
-    assert ctx.behave_driver is driver
-    try:
-        next(gen)
-        pytest.fail('StopIteration expected')
-    except StopIteration:
-        pass
-    assert driver._quit is True
-    assert 'behave_driver' not in ctx
-
-
 def test_driver_with_custom_arguments():
     class CustomDriver(behave_webdriver.fixtures.BehaveDriverMixin):
         def __init__(self, *args, **kwargs):
