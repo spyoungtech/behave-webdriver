f4a8cf6075649eca1aea41cdd70f3331c09b9613
Spencer Young <spencer.young@spyoung.com>
2018-12-08 02:19:40 -0800

    remove tests from old implementation

--- tests/unittests/test_fixture.py
+++ tests/unittests/test_fixture.py
@@ -8,32 +8,6 @@ sys.path.insert(0, root_dir)
 import behave_webdriver.fixtures
 
 
-def test_driver_with_custom_arguments():
-    class CustomDriver(behave_webdriver.fixtures.BehaveDriverMixin):
-        def __init__(self, *args, **kwargs):
-            self._args = args
-            self._kwargs = kwargs
-            self._quit = False
-
-        def quit(self):
-            self._quit = True
-    ctx = mock.MagicMock()
-    gen = behave_webdriver.fixtures.fixture_browser(ctx, 4, True, "test", webdriver=CustomDriver, options={'param': 'value'})
-    driver = next(gen)
-    assert isinstance(driver, CustomDriver)
-    assert ctx.behave_driver is driver
-    assert driver._quit is False
-    assert driver._args == (4, True, "test")
-    assert driver._kwargs == {'options': {'param': 'value'}}
-    try:
-        next(gen)
-        pytest.fail('StopIteration expected')
-    except StopIteration:
-        pass
-    assert driver._quit is True
-    assert 'behave_driver' not in ctx
-
-
 def test_before_all_factory():
     with mock.patch('behave_webdriver.fixtures.use_fixture') as mock_use_fixture:
         args = (4, True, "test")
